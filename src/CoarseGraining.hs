41cfc5674d76223a13505428034ce82f1f1e9670
Max Tyler <mtyler88@gmail.com>
2017-01-31 12:21:42 +0000

    Move stuff from CoarseGrain.hs to CoarseGraining.hs in /src

--- src/CoarseGraining.hs
+++ src/CoarseGraining.hs
@@ -6,5 +6,17 @@ import PDBParser
 data Pair = Pair {atoms :: (Atom, Atom), name :: String, contacts :: Int, rIDs :: (Int, Int)}
   deriving (Show, Eq)
 
+data Element = Carbon | Flourine | Hydrogen |
+               Nitrogen | Oxygen | Sulphur deriving (Show, Eq)
+
+mass :: Fractional a => Element -> a
+mass e = case e of
+  Carbon -> 12.0107
+  Flourine -> 18.9984
+  Hydrogen -> 1.00794
+  Nitrogen -> 14.0067
+  Oxygen -> 15.9994
+  Sulphur -> 32.065
+
 coarseGrainAtoms :: [Atom] -> [Atom]
 coarseGrainAtoms = undefined
