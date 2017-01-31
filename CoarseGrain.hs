93d8a36478f191d0e84d554c7d86ce8853b899b1
Max Tyler <mtyler88@gmail.com>
2017-01-31 00:59:07 +0000

    Add coarsegrain into src folder

--- CoarseGrain.hs
+++ CoarseGrain.hs
@@ -1,4 +1,17 @@
 import CGVector
+import PDBParser
+
+data Element = Carbon | Flourine | Hydrogen |
+               Nitrogen | Oxygen | Sulphur deriving (Show, Eq)
+
+mass :: Fractional a => Element -> a
+mass e = case e of
+  Carbon -> 12.0107
+  Flourine -> 18.9984
+  Hydrogen -> 1.00794
+  Nitrogen -> 14.0067
+  Oxygen -> 15.9994
+  Sulphur -> 32.065
 
 v1 = Vec [2, 3, 4]
 v2 = Vec [2, 3, 2]
@@ -9,11 +22,4 @@ emptyV = Vec [] :: Vec Int
 
 main :: IO ()
 main = do
-  print $ mag v1
-  print $ mag v2
-  print $ mag v1 * mag v2
-  print $ v1 .^ v2
-  print $ (v1 .^ v2) / (mag v1 * mag v2)
-  print $ acos $ (v1 .^ v2) / (mag v1 * mag v2)
-  print $ angle v1 v2
   print $ angleP p1 p2 p3
