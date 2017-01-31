6c4cc8ad4acf4bec07206ed69e8245d1afb096bc
Max Tyler <mtyler88@gmail.com>
2017-01-31 12:17:44 +0000

    Edit coarsegraining

--- src/CoarseGraining.hs
+++ src/CoarseGraining.hs
@@ -2,3 +2,9 @@ module CoarseGraining where
 
 import CGVector
 import PDBParser
+
+data Pair = Pair {atoms :: (Atom, Atom), name :: String, contacts :: Int, rIDs :: (Int, Int)}
+  deriving (Show, Eq)
+
+coarseGrainAtoms :: [Atom] -> [Atom]
+coarseGrainAtoms = undefined
